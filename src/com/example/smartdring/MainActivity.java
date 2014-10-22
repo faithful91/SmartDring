4f2c42c75e1171ed251700d74accaf06dc013317
faithful91 <bradai.hazem@gmail.com>
2014-10-22 14:02:04 +0200

    refac

--- src/com/example/smartdring/MainActivity.java
+++ src/com/example/smartdring/MainActivity.java
@@ -1,90 +1,57 @@
 package com.example.smartdring;
 
-
-
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-import android.media.AudioManager;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.ContentObserver;
-import android.util.Log;
-import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	AudioManager mgr=null;	private ContentObserver mVolumeObserver;
+private	TextView testtxt;
+	private TextView lb;
+	private ListView list1;
+	private	Context e1 = this;
+	private SoundProfile InsSProfile = new SoundProfile(e1);
+	private SoundEdit e = new SoundEdit();
 
-TextView testtxt;
-	TextView lb;
-   ListView list1 ;
-   Context e1=this;
-   SoundProfile InsSProfile=new SoundProfile(e1);
-	  SoundEdit e=new SoundEdit();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	       setContentView(R.layout.list);
+		setContentView(R.layout.list);
 
-	    
-	      
+		lb = (TextView) findViewById(R.id.label);
+		list1 = (ListView) findViewById(R.id.list);
 
-		lb =(TextView)findViewById(R.id.label);
-	       list1 = (ListView) findViewById(R.id.list);
-	       
+		InsSProfile.loadProfiles();
 
-	       InsSProfile.loadProfiles();        
-      
-        
-        MainActivityAdapter adapter = new MainActivityAdapter(this, InsSProfile.Profiles);
-        list1.setAdapter(adapter) ;
-       getWindow().setBackgroundDrawableResource(R.drawable.img2);
-       registerForContextMenu(list1);
-       registerClickCallback();
-       testtxt =(TextView)findViewById(R.id.music);
+		MainActivityAdapter adapter = new MainActivityAdapter(this,
+				InsSProfile.getProfilesNames());
+		list1.setAdapter(adapter);
+		getWindow().setBackgroundDrawableResource(R.drawable.img2);
+		registerForContextMenu(list1);
+		registerClickCallback();
+		testtxt = (TextView) findViewById(R.id.music);
 	}
 
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
-		// TODO Auto-generated method stub
 		super.onCreateContextMenu(menu, v, menuInfo);
-		if (v.getId()==R.id.list) {
-    	    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
-    		menu.setHeaderTitle(InsSProfile.Profiles.get(info.position).getName());}
-		
+		if (v.getId() == R.id.list) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+			menu.setHeaderTitle(InsSProfile.getProfilesNames()
+					.get(info.position).getName());
+		}
 		menu.add("Activer");
 		menu.add("Modifier");
 		menu.add("Delete");
-
 	}
 
 	private void registerClickCallback() {
@@ -93,55 +60,49 @@ TextView testtxt;
 			@Override
 			public void onItemClick(AdapterView<?> parent, View viewClicked,
 					int position, long id) {
-				
-	              viewClicked.showContextMenu();
-					SoundProfile clickedCar = InsSProfile.Profiles.get(position);
 
-				String message = "You clicked position " 
-								+ " Which is car make " + clickedCar.getName();
-				Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+				viewClicked.showContextMenu();
+				SoundProfile clickedCar = InsSProfile.getProfilesNames().get(
+						position);
+
+				String message = "You clicked position "
+						+ " Which is car make " + clickedCar.getName();
+				Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG)
+						.show();
 			}
 		});
 	}
-	
-	
-	
-	  public boolean onContextItemSelected(MenuItem item) {
-          AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
-          if (item.getTitle() == "Activer") {
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
 
-        	 
+		if (item.getTitle() == "Activer") {
 
-        	  
-          }
+		}
 
-          else  if (item.getTitle() == "Delete") {
+		else if (item.getTitle() == "Delete") {
 
-        	  InsSProfile.delete(InsSProfile.Profiles.get(info.position).getName());
-        	  InsSProfile.Profiles.remove(InsSProfile.Profiles.get(info.position));
-        	  list1.invalidateViews();
+			InsSProfile.delete(InsSProfile.getProfilesNames()
+					.get(info.position).getName());
+			InsSProfile.getProfilesNames().remove(
+					InsSProfile.getProfilesNames().get(info.position));
+			list1.invalidateViews();
 
-        	  
-          }
-          else if (item.getTitle() == "Modifier") {
-        	  Toast.makeText(getApplicationContext(), InsSProfile.Profiles.get(info.position).getName(),
-       			   Toast.LENGTH_LONG).show();
-       	  Intent intent = new Intent(this, SoundEdit.class);
-       	  intent.putExtra("sp", InsSProfile.Profiles.get(info.position).getSharedPref());   
-       	  startActivity(intent);
-       	  } else {
-                  return false;
-          }
-          return true;
-	  }
-	  
-	
-	 
+		} else if (item.getTitle() == "Modifier") {
+			Toast.makeText(
+					getApplicationContext(),
+					InsSProfile.getProfilesNames().get(info.position).getName(),
+					Toast.LENGTH_LONG).show();
+			Intent intent = new Intent(this, SoundEdit.class);
+			intent.putExtra("sp",
+					InsSProfile.getProfilesNames().get(info.position)
+							.getSharedPref());
+			startActivity(intent);
+		} else {
+			return false;
+		}
+		return true;
+	}
 
-
-		
-	
-
-		 }
-
+}
--- src/com/example/smartdring/SoundProfile.java
+++ src/com/example/smartdring/SoundProfile.java
@@ -1,152 +1,119 @@
 package com.example.smartdring;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
-
-import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
-public class SoundProfile{
-private String Name;
-private String SharedPref;
-private List<String> profileName = new ArrayList<String>();
-private List<String> profileNameDel = new ArrayList<String>();
+public class SoundProfile {
 
-public List<SoundProfile> Profiles = new ArrayList<SoundProfile>();
-Context fileContext;
-String path;
-public SoundProfile (String Name,String SharedPref ){this.Name=Name;this.SharedPref=SharedPref;} 
-public SoundProfile(Context fileContext) {this.fileContext=fileContext;
-}
-public String getName(){return Name;}
-public String getSharedPref(){return SharedPref;}
+	private String Name;
+	private String SharedPref;
+	private List<String> profileName = new ArrayList<String>();
+	private List<String> profileNameDel = new ArrayList<String>();
+	public List<SoundProfile> Profiles = new ArrayList<SoundProfile>();
+	public Context fileContext;
 
-public void setName(String Name){this.Name=Name;}
-public void setSharedPref(String SharedPref){this.SharedPref=SharedPref;}
-public void loadProfiles(){
+	public SoundProfile(String Name, String SharedPref) {
+		this.Name = Name;
+		this.SharedPref = SharedPref;
+	}
 
+	public SoundProfile(Context fileContext) {
+		this.fileContext = fileContext;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public String getSharedPref() {
+		return SharedPref;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public void setSharedPref(String SharedPref) {
+		this.SharedPref = SharedPref;
+	}
+
+	public List<SoundProfile> getProfilesNames(){return Profiles;} 
 	
+	public void loadProfiles() {
+		readFromFile();
 
+		for (int i = 0; i < profileName.size(); i++)
 
+		{
+			SoundProfile g = new SoundProfile(profileName.get(i),
+					profileName.get(i) + "sp");
+			Profiles.add(g);
+		}
+	}
 
-	
-
-
-
-
-
-	readFromFile();
-
-for (int i=0;i<profileName.size();i++)
-    
-{
-     	SoundProfile g = new SoundProfile(profileName.get(i),profileName.get(i)+"sp");
-         Profiles.add(g);
-     }
-}
-
-//Ecrire sur le fichier pour ajouter un profil
-private void writeToFile(String data) {
-    try {
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileContext.getApplicationContext().openFileOutput("config.txt", Context.MODE_APPEND));
-        outputStreamWriter.write(data);
-        outputStreamWriter.close();
-    }
-    catch (IOException e) {
-        Log.e("Exception", "File write failed: " + e.toString());
-    } 
-}
-//Lire à partir d'un fichier .txt combien de profile existe
-
-private void readFromFile() {
- InputStream inputStream = null;
+	// Ecrire sur le fichier pour ajouter un profil
+	private void writeToFile(String data, int mode) {
 		try {
-			inputStream = fileContext.getApplicationContext().openFileInput("config.txt");
-			} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			Log.e("TAG", "File write failed: " + e.toString());		}
- if ( inputStream != null ) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String receiveString = "";
-            try {
-				while ( (receiveString = bufferedReader.readLine()) != null ) {
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+					fileContext.getApplicationContext().openFileOutput(
+							"config.txt", mode));
+			outputStreamWriter.write(data);
+			outputStreamWriter.close();
+		} catch (IOException e) {
+			Log.e("Exception", "File write failed: " + e.toString());
+		}
+	}
+
+	// Lire à partir d'un fichier .txt combien de profile existe
+	private void readFromFile() {
+		InputStream inputStream = null;
+		try {
+			inputStream = fileContext.getApplicationContext().openFileInput(
+					"config.txt");
+		} catch (FileNotFoundException e) {
+			Log.e("TAG", "File write failed: " + e.toString());
+		}
+		if (inputStream != null) {
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream);
+			BufferedReader bufferedReader = new BufferedReader(
+					inputStreamReader);
+			String receiveString = "";
+			try {
+				while ((receiveString = bufferedReader.readLine()) != null) {
 					profileName.add(receiveString);
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				Log.e("TAG", "File write failed: " + e.toString());			}
-
-            try {
+				Log.e("TAG", "File write failed: " + e.toString());
+			}
+try {
 				inputStream.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
-    
-   
+		}
+	}
 
-  
-}
+	// Delete a profile
+	public void delete(String data) {
+		for (int i = 0; i < profileName.size(); i++) {
+			if (!profileName.get(i).equals(data))
+				profileNameDel.add(profileName.get(i));}
+		writeToFile("", Context.MODE_PRIVATE);
 
-
-
-public void delete(String data) {
-	for (int i=0;i<profileName.size();i++)
-	    
-	{
-if (!profileName.get(i).equals(data)) profileNameDel.add(profileName.get(i));
-			}writeToFileDel("");
-
-	for (int j=0;j<profileNameDel.size();j++)
-	{	
-	writeToFile(profileNameDel.get(j));
-	writeToFile(	       System.getProperty("line.separator"));}
-
-	Log.e("TAG", "tayarto");
-	
-
-	
-	        }
-
-
-public void removeLine(final File file, final int lineIndex) throws IOException{
-    final List<String> lines = new LinkedList<String>();
-    
-    final Scanner reader = new Scanner(fileContext.getApplicationContext().openFileInput("config.txt"));
-    while(reader.hasNextLine())
-        lines.add(reader.nextLine());
-    reader.close();
-    assert lineIndex >= 0 && lineIndex <= lines.size() - 1;
-    lines.remove(lineIndex);
-    final BufferedWriter writer = new BufferedWriter(new FileWriter(file, false));
-    for(final String line : lines)
-        writer.write(line);
-    writer.flush();
-    writer.close();
-}
-private void writeToFileDel(String data) {
-    try {
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileContext.getApplicationContext().openFileOutput("config.txt", Context.MODE_PRIVATE));
-        outputStreamWriter.write(data);
-        outputStreamWriter.close();
-    }
-    catch (IOException e) {
-        Log.e("Exception", "File write failed: " + e.toString());
-    } 
-}
+		for (int j = 0; j < profileNameDel.size(); j++) {
+			writeToFile(profileNameDel.get(j), Context.MODE_APPEND);
+			writeToFile(System.getProperty("line.separator"),
+					Context.MODE_APPEND);
+		}
+	}
 }
